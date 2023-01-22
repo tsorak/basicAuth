48384972eb7755787fcbbffd530e74b4e6f89f85
tsorak <karost0249@gmail.com>
2023-01-22 16:07:21 +0100

    Remove restrictions for ease of testability

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -6,20 +6,20 @@ import { db } from "~/db";
 import { createUserSession, getUser, login, register } from "~/db/session";
 
 function validateUsername(username: unknown) {
-  if (typeof username !== "string" || username.length < 3) {
-    return `Usernames must be at least 3 characters long`;
+  if (typeof username !== "string" || username.length < 1) {
+    return `Usernames must be at least 1 characters long`;
   }
 }
 
 function validatePassword(password: unknown) {
-  if (typeof password !== "string" || password.length < 6) {
-    return `Passwords must be at least 6 characters long`;
+  if (typeof password !== "string" || password.length < 1) {
+    return `Passwords must be at least 1 characters long`;
   }
 }
 
 export function routeData() {
   return createServerData$(async (_, { request }) => {
-    if (await getUser(db, request)) {
+    if (await getUser(request)) {
       throw redirect("/");
     }
     return {};
